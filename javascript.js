124041a534f23a1bbb6ef8548a269ee1e9280283
VorahPong <vorahpongmean@gmail.com>
2024-08-04 00:06:47 -0500

    modified javascript.js
    add a draw function to change the cell color when clicking them.

--- javascript.js
+++ javascript.js
@@ -7,3 +7,16 @@ for(let index = 0; index < BOARD_SIZE; index++){
     newDiv.setAttribute('id', 'pixel');
     container.appendChild(newDiv);
 }
+
+const pixel = document.querySelectorAll('#pixel');
+
+
+for(let pixel_index = 0; pixel_index < pixel.length; pixel_index++){
+    pixel[pixel_index].addEventListener('click', function (e) {
+        if(e.target.style.backgroundColor === 'black'){
+            e.target.style.backgroundColor = 'transparent';
+        }
+        else{
+            e.target.style.backgroundColor = 'black';
+        }
+})};
